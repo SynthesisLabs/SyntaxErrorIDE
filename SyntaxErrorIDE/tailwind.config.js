2bb0442f6ce3fc0e37294f95618ed1cb72016ccc
dysshanks <ryanvdvorst@outlook.com>
2025-04-11 10:21:57 +0200

    fix tailwind

---
+++ SyntaxErrorIDE/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./Pages/**/*.{cshtml,cshtml.cs}",
+        "./app/**/*.{cshtml,cshtml.cs}",
+        "./wwwroot/**/*.{html,cshtml}",
+        "./**/*.{cshtml}"
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [],
+}
